c950bb13b12874dad11b153af72042cf9772e834
SergeyZderev <75411616+Brendj@users.noreply.github.com>
2024-08-21 11:59:30 +0300

    предобработка JSON файла

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,10 +17,14 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         try {
+            String filePath = "tickets.json";
+            String jsonContent = readFile(filePath);
+            String processedJsonContent = preprocessJson(jsonContent);
+
             ObjectMapper mapper = new ObjectMapper();
             mapper.registerModule(new JavaTimeModule());
 
-            Tickets tickets = mapper.readValue(new File("tickets.json"), Tickets.class);
+            Tickets tickets = mapper.readValue(processedJsonContent, Tickets.class);
 
             List<Flight> ticketList = tickets.getTickets();
             List<Double> prices = new ArrayList<>();
@@ -28,13 +35,14 @@ public class Main {
                 if (ticket.getOriginName().equals("Владивосток") && ticket.getDestinationName().equals("Тель-Авив")) {
                     prices.add((double) ticket.getPrice());
 
+                    LocalDateTime departureDateTime = combineDateAndTime(ticket.getDepartureDate(), ticket.getDepartureTime());
+                    LocalDateTime arrivalDateTime = combineDateAndTime(ticket.getArrivalDate(), ticket.getArrivalTime());
+
                     if ((minTimeFlightCorp.get(ticket.getCarrier()) == null) ||
-                            differenceLocalDateTimes(combineDateAndTime(ticket.getDepartureDate(), ticket.getDepartureTime()),
-                                                        combineDateAndTime(ticket.getArrivalDate(), ticket.getArrivalTime()))
+                            differenceLocalDateTimes(departureDateTime, arrivalDateTime)
                                     .isBefore(minTimeFlightCorp.get(ticket.getCarrier()))) {
-                            minTimeFlightCorp.put(ticket.getCarrier(), differenceLocalDateTimes(combineDateAndTime(ticket.getDepartureDate(), ticket.getDepartureTime()),
-                                                                                                combineDateAndTime(ticket.getArrivalDate(), ticket.getArrivalTime())));
-                        }
+                        minTimeFlightCorp.put(ticket.getCarrier(), differenceLocalDateTimes(departureDateTime, arrivalDateTime));
+                    }
                 }
             }
 
@@ -61,14 +69,23 @@ public class Main {
         }
     }
 
+    public static String readFile(String path) throws IOException {
+        String content = new String(Files.readAllBytes(Paths.get(path)));
+        if (content.startsWith("\uFEFF")) {
+            content = content.substring(1);
+        }
+        return content;
+    }
+
+    public static String preprocessJson(String jsonContent) {
+        return jsonContent.replaceAll("(?<!\\d)(\\d):(\\d{2})(?!\\d)", "0$1:$2");
+    }
+
     public static LocalDateTime differenceLocalDateTimes(LocalDateTime dateTime1, LocalDateTime dateTime2) {
         Duration duration = Duration.between(dateTime1, dateTime2);
-
         LocalDate zeroDate = LocalDate.of(0, 1, 1);
         LocalDateTime zeroDateTime = LocalDateTime.of(zeroDate, LocalTime.MIDNIGHT);
-
         zeroDateTime = zeroDateTime.plus(duration);
-
         return zeroDateTime;
     }
 
@@ -92,9 +109,7 @@ public class Main {
 
     public static double calculateMedian(List<Double> prices) {
         Collections.sort(prices);
-
         int length = prices.size();
-
         if (length % 2 == 0) {
             return (prices.get(length / 2 - 1) + prices.get(length / 2)) / 2.0;
         } else {
